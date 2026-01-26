1e78b8deefd2b521788abd8f898942d1b7943e38
Rushabh Shah <rushabh1404@gmail.com>
2026-01-26 17:32:47 -0500

    site update #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,51 @@
 var gulp = require('gulp');
-var path = require('path');
 var sass = require('gulp-sass')(require('sass'));
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-var open = require('gulp-open');
+const browserSync = require('browser-sync').create();
 
 var Paths = {
   HERE: './',
-  DIST: 'dist/',
   CSS: './assets/css/',
-  SCSS_TOOLKIT_SOURCES: './assets/scss/material-kit.scss',
-  SCSS: './assets/scss/**/**'
+  SCSS_SOURCES: './assets/scss/material-kit.scss',
+  SCSS_ALL: './assets/scss/**/**',
+  HTML: './**/*.html'
 };
 
-gulp.task('compile-scss', function() {
-  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+// 1. Compile SCSS and inject into browser
+function compileScss() {
+  return gulp.src(Paths.SCSS_SOURCES)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write(Paths.HERE))
-    .pipe(gulp.dest(Paths.CSS));
-});
+    .pipe(gulp.dest(Paths.CSS))
+    .pipe(browserSync.stream()); // This updates CSS without a full page refresh
+}
 
-gulp.task('watch', function() {
-  gulp.watch(Paths.SCSS, gulp.series('compile-scss'));
-});
+// 2. Initialize the Server
+function serve(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000,
+    notify: false,
+    ghostMode: false // Prevents multiple tabs from scrolling together
+  });
+  done();
+}
 
-gulp.task('open', function() {
-  gulp.src('index.html')
-    .pipe(open());
-});
+// 3. Watch for changes
+function watchFiles() {
+  gulp.watch(Paths.SCSS_ALL, compileScss);
+  gulp.watch(Paths.HTML).on('change', browserSync.reload);
+  gulp.watch("./assets/js/**/*.js").on('change', browserSync.reload);
+}
 
-gulp.task('open-app', gulp.parallel('open', 'watch'));
+// 4. Define the Task
+// This will compile everything, start the server, and watch for changes
+const build = gulp.series(compileScss, gulp.parallel(serve, watchFiles));
+
+exports['open-app'] = build;
+exports.default = build;
